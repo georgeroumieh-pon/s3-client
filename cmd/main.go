f37041700c84ab9990c73af9a51e2592f2e861d7
georgeroumieh-pon <george.roumieh@PON>
2025-04-24 19:16:28 +0200

    add comments

--- cmd/main.go
+++ cmd/main.go
@@ -10,23 +10,29 @@ import (
 )
 
 func main() {
+	// Initialize logger
 	encoderConfig := ecszap.NewDefaultEncoderConfig()
 	core := ecszap.NewCore(encoderConfig, os.Stdout, zap.DebugLevel)
 	log := zap.New(core, zap.AddCaller())
-
+	// Create a new S3 client
 	s3Client, err := client.CreateS3Client()
 	if err != nil {
 		log.Sugar().Error(err)
+		return
 	}
+	// Create a new bucket
 	bucketName, err := storage.CreateBucket(log, s3Client.Ctx, s3Client.Client)
 	if err != nil {
 		log.Sugar().Error(err)
 	}
+	// Upload files to the bucket
 	err = storage.UploadFiles(log, s3Client.Ctx, s3Client.Client, bucketName)
 	if err != nil {
 		log.Sugar().Error(err)
 	}
-	err = storage.DownloadFile(log, s3Client.Client, bucketName, "object1.txt")
+	filesToDownload := []string{"object1.txt", "object2.txt", "object3.txt"}
+	// Download a file from the bucket
+	err = storage.DownloadFile(log, s3Client.Client, bucketName, filesToDownload)
 	if err != nil {
 		log.Sugar().Error(err)
 	}
