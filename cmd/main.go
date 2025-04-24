6b7eb2b745b0388c2ea57c3a90da15a225b0d2df
georgeroumieh-pon <george.roumieh@PON>
2025-04-24 18:35:27 +0200

    add logger

--- cmd/main.go
+++ cmd/main.go
@@ -1,24 +1,33 @@
 package main
 
 import (
-	"log"
+	"os"
 
 	"github.com/georgeroumieh-pon/go-client/pkg/client"
 	"github.com/georgeroumieh-pon/go-client/pkg/storage"
+	"go.elastic.co/ecszap"
+	"go.uber.org/zap"
 )
 
 func main() {
+	encoderConfig := ecszap.NewDefaultEncoderConfig()
+	core := ecszap.NewCore(encoderConfig, os.Stdout, zap.DebugLevel)
+	log := zap.New(core, zap.AddCaller())
+
 	s3Client, err := client.CreateS3Client()
 	if err != nil {
-		log.Fatal(err)
+		log.Sugar().Error(err)
 	}
-	bucketName := storage.CreateBucket(s3Client.Ctx, s3Client.Client)
-	err = storage.UploadFiles(s3Client.Ctx, s3Client.Client, bucketName)
+	bucketName, err := storage.CreateBucket(log, s3Client.Ctx, s3Client.Client)
 	if err != nil {
-		log.Fatal(err)
+		log.Sugar().Error(err)
 	}
-	err = storage.DownloadFile(s3Client.Client, bucketName, "object1.txt")
+	err = storage.UploadFiles(log, s3Client.Ctx, s3Client.Client, bucketName)
 	if err != nil {
-		log.Fatal(err)
+		log.Sugar().Error(err)
+	}
+	err = storage.DownloadFile(log, s3Client.Client, bucketName, "object1.txt")
+	if err != nil {
+		log.Sugar().Error(err)
 	}
 }
