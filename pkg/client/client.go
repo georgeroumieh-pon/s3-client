8505878e623de9f2e7a5d1d9343800d587928412
georgeroumieh-pon <george.roumieh@PON>
2025-04-24 18:19:40 +0200

    update client

--- pkg/client/client.go
+++ pkg/client/client.go
@@ -2,11 +2,11 @@ package client
 
 import (
 	"context"
+	"fmt"
 	"os"
 
-	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
-	awscred "github.com/aws/aws-sdk-go-v2/credentials"
+	"github.com/aws/aws-sdk-go-v2/credentials"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 )
 
@@ -14,37 +14,33 @@ const (
 	endpoint     string = "http://localhost:9000"
 	accessKeyEnv string = "ACCESS_KEY"
 	secretKeyEnv string = "SECRET_KEY"
+	region       string = "eu-west-4"
 )
 
 // CreateS3Client returns a MinIO-compatible S3 client
-func CreateS3Client() (s3Client S3Client, err error) {
+func CreateS3Client() (s3Client *S3Client, err error) {
 	ctx := context.Background()
 	// Get MinIO credentials from environment variables
-	accessKeyID := os.Getenv(accessKeyEnv)
-	secretAccessKey := os.Getenv(secretKeyEnv)
-
-	customResolver := aws.EndpointResolverWithOptionsFunc(
-		func(service, region string, options ...interface{}) (aws.Endpoint, error) {
-			return aws.Endpoint{
-				URL:           endpoint,
-				SigningRegion: "eu-west-3",
-			}, nil
-		})
+	accessKey := os.Getenv(accessKeyEnv)
+	secretKey := os.Getenv(secretKeyEnv)
+	if accessKey == "" || secretKey == "" {
+		return nil, fmt.Errorf("missing ACCESS_KEY or SECRET_KEY environment variables")
+	}
 
 	cfg, err := config.LoadDefaultConfig(context.TODO(),
-		config.WithRegion("eu-west-3"),
-		config.WithEndpointResolverWithOptions(customResolver),
-		config.WithCredentialsProvider(awscred.NewStaticCredentialsProvider(accessKeyID, secretAccessKey, "")),
+		config.WithRegion(region),
+		config.WithCredentialsProvider(credentials.NewStaticCredentialsProvider(accessKey, secretKey, "")),
+		config.WithBaseEndpoint(endpoint),
 	)
 	if err != nil {
-		return s3Client, err
+		return nil, err
 	}
 
 	client := s3.NewFromConfig(cfg, func(o *s3.Options) {
 		o.UsePathStyle = true
 	})
 
-	s3Client = S3Client{
+	s3Client = &S3Client{
 		Client: client,
 		Ctx:    ctx,
 	}
